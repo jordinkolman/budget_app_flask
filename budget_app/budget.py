47513e8330bed3069f507122f73834da98f74255
Jordin Kolman <jordinkolman@gmail.com>
2023-12-31 12:01:59 -0600

    added account updating and deletion functionality

--- budget_app/budget.py
+++ budget_app/budget.py
@@ -12,7 +12,7 @@ def index():
     print(g)
     db = get_db()
     accounts = (db.execute(
-        'SELECT name, balance, account_type FROM account WHERE user_id = ?',
+        'SELECT id, name, balance, account_type FROM account WHERE user_id = ?',
         (g.user["id"],)
     ).fetchall())
 
@@ -27,6 +27,26 @@ def index():
     ).fetchall())
 
     return render_template('budget/index.html', accounts=accounts, transactions=transactions)
+
+def get_account(id, check_user=True):
+    account = (
+        get_db()
+        .execute(
+            'SELECT a.id, name, balance, account_type, user_id, username'
+            ' FROM account a JOIN user u ON a.user_id = u.id'
+            ' WHERE a.id = ?',
+            (id,),
+        )
+        .fetchone()
+    )
+
+    if account is None:
+        abort(404, f"Account ID {id} doesn't exist")
+
+    if check_user and account["id"] != g.user['id']:
+        abort(403)
+
+    return account
 
 
 @bp.route('/add_account', methods=("GET", "POST"))
@@ -58,3 +78,44 @@ def add_account():
             return redirect(url_for('budget.index'))
 
     return render_template('budget/add_account.html')
+
+@bp.route('/<int:id>/update_account', methods=("GET", "POST"))
+@login_required
+def update_account(id):
+    account = get_account(id)
+
+    if request.method == 'POST':
+        name = request.form['name']
+        balance = request.form['balance']
+        account_type = request.form['account_type']
+        error = None
+
+        if not name:
+            error = "Account Name is Required."
+        elif not account_type:
+            error = "Account Type is Required."
+
+        if not balance:
+            balance = 0.0
+
+        if error is not None:
+            flash(error)
+        else:
+            db = get_db()
+            db.execute(
+                "UPDATE account SET name = ?, balance = ?, account_type = ? WHERE id = ?",
+                (name, balance, account_type, id)
+            )
+            db.commit()
+            return redirect(url_for('budget.index'))
+
+    return render_template('budget/update_account.html', account=account)
+
+@bp.route("/<int:id>/delete_account", methods=("POST",))
+@login_required
+def delete_account(id):
+    get_account(id)
+    db = get_db()
+    db.execute("DELETE FROM account WHERE id = ?", (id,))
+    db.commit()
+    return redirect(url_for('budget.index'))
