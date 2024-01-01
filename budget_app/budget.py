5374dd9b9c9f88dcb2ae3022dd7db84b32820822
Jordin Kolman <jordinkolman@gmail.com>
2024-01-01 15:47:30 -0600

    implemented transaction deletion capability

--- budget_app/budget.py
+++ budget_app/budget.py
@@ -4,14 +4,15 @@ from werkzeug.exceptions import abort
 from budget_app.auth import login_required
 from budget_app.db import get_db
 
-bp = Blueprint('budget', __name__)
+bp = Blueprint("budget", __name__)
+
 
 def get_id(title, table):
     db = get_db()
     table_vals = []
 
-    if table in ['vendors', 'categories']:
-        query = f'SELECT name FROM {table}'
+    if table in ["vendors", "categories"]:
+        query = f"SELECT name FROM {table}"
         table_rows = db.execute(query).fetchall()
         for row in table_rows:
             table_vals.append(row[0])
@@ -28,37 +29,39 @@ def get_id(title, table):
     return db.execute(query).fetchone()[0]
 
 
-@bp.route('/', methods=("GET", "POST"))
+@bp.route("/", methods=("GET", "POST"))
 @login_required
 def index():
     db = get_db()
     if request.method == "POST":
-        date = request.form['date']
-        time = request.form['time']
-        vendor = request.form['vendor']
-        category = request.form['category']
-        transaction_type = request.form['transaction_type']
-        amount = request.form['amount']
-        account = request.form['account']
+        date = request.form["date"]
+        time = request.form["time"]
+        vendor = request.form["vendor"]
+        category = request.form["category"]
+        transaction_type = request.form["transaction_type"]
+        amount = request.form["amount"]
+        account = request.form["account"]
         error = None
 
         if not date:
-            error = 'Date is Required'
+            error = "Date is Required"
         elif not time:
-            error = 'Time is Required'
+            error = "Time is Required"
         elif not vendor:
-            error = 'Vendor is Required'
+            error = "Vendor is Required"
         elif not category:
-            error = 'Category is Required'
+            error = "Category is Required"
         elif not amount:
             amount = 0.0
 
-        accounts = db.execute('SELECT * FROM account WHERE user_id = ? ORDER BY name', (g.user['id'],)).fetchall()
+        accounts = db.execute(
+            "SELECT * FROM account WHERE user_id = ? ORDER BY name", (g.user["id"],)
+        ).fetchall()
         print(accounts)
         account_id = None
         for acc in accounts:
             if acc[2] == account:
-                account_id = acc['id']
+                account_id = acc["id"]
 
         if account_id is None:
             error = "Invalid Account. Please Enter a Valid Account or Add a New One."
@@ -66,44 +69,64 @@ def index():
         if error is not None:
             flash(error)
         else:
-            category_id = get_id(title=category, table='categories')
-            vendor_id = get_id(title=vendor, table='vendors')
-            type_id = 1 if transaction_type == 'Income' else 2
+            category_id = get_id(title=category, table="categories")
+            vendor_id = get_id(title=vendor, table="vendors")
+            type_id = 1 if transaction_type == "Income" else 2
 
             db.execute(
-                'INSERT INTO transactions (user_id, date_occurred, time_occurred, vendor_id, category_id, type_id, amount, account_id)'
-                ' VALUES (?, ?, ?, ?, ?, ?, ?, ?)',
-                (g.user['id'], date, time, vendor_id, category_id, type_id, amount, account_id,)
+                "INSERT INTO transactions (user_id, date_occurred, time_occurred, vendor_id, category_id, type_id, amount, account_id)"
+                " VALUES (?, ?, ?, ?, ?, ?, ?, ?)",
+                (
+                    g.user["id"],
+                    date,
+                    time,
+                    vendor_id,
+                    category_id,
+                    type_id,
+                    amount,
+                    account_id,
+                ),
             )
             db.commit()
-            return redirect(url_for('budget.index'))
+            return redirect(url_for("budget.index"))
+
+    accounts = db.execute(
+        "SELECT id, name, balance, account_type FROM account WHERE user_id = ?",
+        (g.user["id"],),
+    ).fetchall()
+
+    transactions = db.execute(
+        "SELECT t.id, date_occurred, time_occurred, v.name v_name, c.name c_name, ty.name ty_name, amount, a.name a_name"
+        " FROM transactions t JOIN vendors v ON t.vendor_id = v.id"
+        "  JOIN categories c ON t.category_id = c.id"
+        "  JOIN types ty ON t.type_id = ty.id"
+        "  JOIN account a ON t.account_id = a.id"
+        "  WHERE t.user_id = ?",
+        (g.user["id"],),
+    ).fetchall()
+
+    return render_template(
+        "budget/index.html", accounts=accounts, transactions=transactions,
+    )
 
 
-    accounts = (db.execute(
-        'SELECT id, name, balance, account_type FROM account WHERE user_id = ?',
-        (g.user["id"],)
-    ).fetchall())
 
+@bp.route("/<int:id>/delete_transaction", methods=("POST",))
+@login_required
+def delete_transaction(id):
+    db = get_db()
+    db.execute("DELETE FROM transactions WHERE id = ?", (id,))
+    db.commit()
+    return redirect(url_for("budget.index"))
 
-    transactions = (db.execute(
-        'SELECT date_occurred, time_occurred, v.name v_name, c.name c_name, ty.name ty_name, amount, a.name a_name'
-        ' FROM transactions t JOIN vendors v ON t.vendor_id = v.id'
-        '  JOIN categories c ON t.category_id = c.id'
-        '  JOIN types ty ON t.type_id = ty.id'
-        '  JOIN account a ON t.account_id = a.id'
-        '  WHERE t.user_id = ?',
-        (g.user["id"],)
-    ).fetchall())
-
-    return render_template('budget/index.html', accounts=accounts, transactions=transactions)
 
 def get_account(id, check_user=True):
     account = (
         get_db()
         .execute(
-            'SELECT a.id, name, balance, account_type, user_id, username'
-            ' FROM account a JOIN user u ON a.user_id = u.id'
-            ' WHERE a.id = ?',
+            "SELECT a.id, name, balance, account_type, user_id, username"
+            " FROM account a JOIN user u ON a.user_id = u.id"
+            " WHERE a.id = ?",
             (id,),
         )
         .fetchone()
@@ -112,19 +135,19 @@ def get_account(id, check_user=True):
     if account is None:
         abort(404, f"Account ID {id} doesn't exist")
 
-    if check_user and account["id"] != g.user['id']:
+    if check_user and account["user_id"] != g.user["id"]:
         abort(403)
 
     return account
 
 
-@bp.route('/add_account', methods=("GET", "POST"))
+@bp.route("/add_account", methods=("GET", "POST"))
 @login_required
 def add_account():
     if request.method == "POST":
-        name = request.form['name']
-        balance = request.form['balance']
-        account_type = request.form['account_type']
+        name = request.form["name"]
+        balance = request.form["balance"]
+        account_type = request.form["account_type"]
         error = None
 
         if not name:
@@ -141,22 +164,28 @@ def add_account():
             db = get_db()
             db.execute(
                 "INSERT INTO account (user_id, name, balance, account_type) VALUES (?, ?, ?, ?)",
-                (g.user['id'], name, balance, account_type,)
+                (
+                    g.user["id"],
+                    name,
+                    balance,
+                    account_type,
+                ),
             )
             db.commit()
-            return redirect(url_for('budget.index'))
+            return redirect(url_for("budget.index"))
 
-    return render_template('budget/add_account.html')
+    return render_template("budget/add_account.html")
 
-@bp.route('/<int:id>/update_account', methods=("GET", "POST"))
+
+@bp.route("/<int:id>/update_account", methods=("GET", "POST"))
 @login_required
 def update_account(id):
     account = get_account(id)
 
-    if request.method == 'POST':
-        name = request.form['name']
-        balance = request.form['balance']
-        account_type = request.form['account_type']
+    if request.method == "POST":
+        name = request.form["name"]
+        balance = request.form["balance"]
+        account_type = request.form["account_type"]
         error = None
 
         if not name:
@@ -173,12 +202,13 @@ def update_account(id):
             db = get_db()
             db.execute(
                 "UPDATE account SET name = ?, balance = ?, account_type = ? WHERE id = ?",
-                (name, balance, account_type, id)
+                (name, balance, account_type, id),
             )
             db.commit()
-            return redirect(url_for('budget.index'))
+            return redirect(url_for("budget.index"))
 
-    return render_template('budget/update_account.html', account=account)
+    return render_template("budget/update_account.html", account=account)
+
 
 @bp.route("/<int:id>/delete_account", methods=("POST",))
 @login_required
@@ -187,4 +217,4 @@ def delete_account(id):
     db = get_db()
     db.execute("DELETE FROM account WHERE id = ?", (id,))
     db.commit()
-    return redirect(url_for('budget.index'))
+    return redirect(url_for("budget.index"))
